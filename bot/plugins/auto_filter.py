3970750d40e4e9e0398f75de77639cfa4aad8b48
shivaprasad123k <92025182+shivaprasad123k@users.noreply.github.com>
2021-10-26 12:32:41 +0530

    Update auto_filter.py

--- bot/plugins/auto_filter.py
+++ bot/plugins/auto_filter.py
@@ -129,8 +129,10 @@ async def auto_filter(bot: Client, update: Message):
         
     else:
         Send_message = await bot.send_photo( chat_id=update.chat.id,
- photo="https://telegra.ph/file/eabeaf432b58f129ea2b4.jpg",
- caption="<b>Couldn't Find This Movie.Try Again..! ഈ സിനിമയുടെ ഒറിജിനൽ പേര് ഗൂഗിളിൽ പോയി കണ്ടെത്തി അതുപോലെ ഇവിടെ കൊടുക്കുക 🥺</b>", reply_to_message_id=update.message_id )
+            photo="https://telegra.ph/file/eabeaf432b58f129ea2b4.jpg",
+            caption="<b>Couldn't Find This Movie.Try Again..! ഈ സിനിമയുടെ ഒറിജിനൽ പേര് ഗൂഗിളിൽ പോയി കണ്ടെത്തി അതുപോലെ ഇവിടെ കൊടുക്കുക 🥺</b>",
+            reply_to_message_id=update.message_id )
+        
         await asyncio.sleep(20)
         await Send_message.delete()
         return # return if no files found for that query
