2d55858976932fa851099183c4fe1b0a32a8c3c0
Peaky blinder <85233139+PEAKY-BLINDER-TG@users.noreply.github.com>
2021-10-27 15:16:09 +0530

    Update welcome.py

--- bot/plugins/welcome.py
+++ bot/plugins/welcome.py
@@ -1,1 +1,14 @@
+import os
+from pyrogram import Client as wasim
+from pyrogram.types import Message, User
 
+@wasim.on_message(filters.new_chat_members)
+async def welcome(bot,message):
+	chatid= message.chat.id
+	await bot.send_message(text=f"**Hey there {message.from_user.mention}, welcome to {message.chat.username} How are you 🥰**",chat_id=chatid)
+	
+@wasim.on_message(filters.left_chat_member)
+async def goodbye(bot,message):
+	chatid= message.chat.id
+	await bot.send_message(text=f"Bye ,  {message.from_user.mention} , Have a Nice Day",chat_id=chatid)
+	
